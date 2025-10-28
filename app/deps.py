9cfa243ade8c15ef5cffd2a59d912cb9844978cf
김현송 <kimhyss041@gmail.com>
2025-10-28 16:40:56 +0900

    DB 구축 및 의존성, 데이터 vector화 코드

--- app/deps.py
+++ app/deps.py
@@ -1,1 +1,5 @@
-# DB/벡터DB 의존성 주입 자리표시자
+# DB를 매일 새로 연결할 필요없이 자동으로 함수 실행해서 DB 객체를 db에 넣어주는 py
+from app.vector.chroma_client import get_chroma_collection
+
+def get_vectorstore(): 
+    return get_chroma_collection()
